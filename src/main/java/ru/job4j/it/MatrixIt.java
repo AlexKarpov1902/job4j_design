b110e1dc7e058db87aa06a44ab563847bdbab1f3
AlexKarpov1902 <leb34359@gmail.com>
2020-12-11 21:01:05 +0500

    Add MatrixIt & MatrixItTest

---
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -0,0 +1,47 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIt implements Iterator<Integer> {
+
+    private final int[][] data;
+    private int row = 0;
+    private int column = -1;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        int c = column;
+        int r = row;
+        while (true) {
+            if (data[r].length <= c + 1) {
+                c = -1;
+                r++;
+            } else {
+                return true;
+            }
+            if (data.length <= r) {
+                return false;
+            }
+        }
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        while (true) {
+            if (data[row].length <= ++column) {
+                column = -1;
+                row++;
+            } else {
+                return data[row][column];
+            }
+        }
+    }
+}
