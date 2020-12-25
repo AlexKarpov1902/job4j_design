e106b071fa70f87c2ae5ac154b23bfe06cc842d7
AlexKarpov1902 <leb34359@gmail.com>
2020-12-25 20:58:58 +0500

    FileOutputStream

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,10 +5,10 @@ import java.io.FileOutputStream;
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write("Таблица умножения \r\n\r\n".getBytes());
+            out.write(("Таблица умножения " + System.lineSeparator()).getBytes());
             for (int i = 1; i <= 10; i++) {
                 for (int j = 1; j <= 10; j++) {
-                    String s = "  " + i + " * " + j + " = " + (j * i) + "\r\n";
+                    String s = "  " + i + " * " + j + " = " + (j * i) + System.lineSeparator();
                     out.write(s.getBytes());
                 }
             }
