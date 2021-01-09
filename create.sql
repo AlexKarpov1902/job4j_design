0c6a3c08f639cad4fc4652623dfac297b83ae765
AlexKarpov1902 <leb34359@gmail.com>
2021-01-09 12:57:14 +0500

    1. Inner join [#428353]

--- create.sql
+++ create.sql
@@ -5,7 +5,7 @@ CREATE TABLE roles (id serial primary key,
                     role_name varchar(100));
 CREATE TABLE rules (id serial primary key, rule varchar(100) );
 CREATE TABLE rule_role (id serial primary key, rule_id int references rules(id),
-                        role_id int references rules(id));
+                        role_id int references roles(id));
 CREATE TABLE items (id serial primary key, items_num int, user_id int references users(id));
 CREATE TABLE comments (id serial primary key, note text, item_id int references items(id));
 CREATE TABLE attachs (id serial primary key, files text, item_id int references items(id));
