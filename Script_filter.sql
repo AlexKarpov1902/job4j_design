44383b2475951239a2043bb517c34f21c4cc5c86
AlexKarpov1902 <leb34359@gmail.com>
2021-01-10 17:33:40 +0500

    Фильтры [#428355]

--- Script_filter.sql
+++ Script_filter.sql
@@ -1,7 +1,7 @@
 select t.name, p.* from product  p join type  t on p.type_id = t.id where t.name = 'СЫР';
 select t.name, p.* from product  p join type  t on p.type_id = t.id where p.name LIKE '%мороженое%';
-select t.name, count(p.*) from product  p join type  t on p.type_id = t.id
-    where p.expired_date between '01.02.2021' and '28.02.2021';
+select t.name, p.* from product  p join type  t on p.type_id = t.id
+where p.expired_date between current_date and current_date + interval '1 month';
 select  * from product where price = (select max(price) from product);
 select t.name, count(p.*) from product p
     join type  t on p.type_id = t.id
